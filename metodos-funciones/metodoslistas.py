d4065373cfd73042d302c2706a92394f6987fe10
nereasaga <sgm.nerea@gmail.com>
2025-03-26 14:24:14 +0100

    añadido extend a listas

--- metodos-funciones/metodoslistas.py
+++ metodos-funciones/metodoslistas.py
@@ -19,25 +19,30 @@ print(lista)  # [1, 2, 3, 4, 2, 2, 5]
 lista.insert(1, 2)
 print(lista)  # [1, 2, 2, 3, 4, 2, 2, 5]
 
+# extend(x, y) Agrega varios elementos al final de la lista
+lista.extend([5, 6, 7]) 
+print(lista) #  [1, 2, 2, 3, 4, 2, 2, 5, 5, 6, 7]
+
 # remove(x)	Elimina la primera aparición de x
 lista.remove(2)
-print(lista)  # [1, 2, 3, 4, 2, 2, 5]
+print(lista)  # [1, 2, 3, 4, 2, 2, 5, 5, 6, 7]
 
 # sort()	Ordena los elementos de menor a mayor
 lista.sort()
-print(lista)  # [1, 2, 2, 2, 3, 4, 5]
+print(lista)  # [1, 2, 2, 2, 3, 4, 5, 5, 6, 7]
 
 # reverse()	Invierte el orden de la lista
 lista.reverse()
-print(lista)  # [5, 4, 3, 2, 2, 2, 1]
+print(lista)  # [7, 6, 5, 5, 4, 3, 2, 2, 2, 1]
 
-print(lista[::-1])  # [1, 2, 2, 2, 3, 4, 5] (porque se ha invertido 2 veces)
+print(lista[::-1])  # [1, 2, 2, 2, 3, 4, 5, 5, 6, 7] (porque se ha invertido 2 veces)
 
 # slice(start, stop, step)	Extrae un rango de la lista
+lista = [1, 2, 2, 2, 3, 4, 5, 5, 6, 7]
 
 # Crear un objeto slice(start, stop, step)
 s = slice(1, 5, 2)
-print(lista[s])  # [4, 2]
+print(lista[s])  # [6, 5] (Elementos desde el índice 1 hasta el 5, saltando de 2 en 2)
 
 
 lista2 = [10, 20, 30, 40, 50, 60]
